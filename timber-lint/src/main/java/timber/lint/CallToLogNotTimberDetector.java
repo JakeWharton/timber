8076866b42eb120f26b54ff4907fae4ad48d0a13
Jake Wharton <jw@squareup.com>
2014-01-11 02:50:06 -0800

    Formatting stuffs. Getting build to pass checkstyle.

--- timber-lint/src/main/java/timber/lint/CallToLogNotTimberDetector.java
+++ timber-lint/src/main/java/timber/lint/CallToLogNotTimberDetector.java
@@ -14,28 +14,24 @@ import java.util.List;
 import lombok.ast.AstVisitor;
 import lombok.ast.MethodInvocation;
 
-public class CallToLogNotTimberDetector extends Detector implements Detector.JavaScanner {
-
-  @Override
-  public List<String> getApplicableMethodNames() {
+final class CallToLogNotTimberDetector extends Detector implements Detector.JavaScanner {
+  @Override public List<String> getApplicableMethodNames() {
     return Arrays.asList("d", "i", "w", "e");
   }
 
   @Override
   public void visitMethod(JavaContext context, AstVisitor visitor, MethodInvocation node) {
     if (node.toString().startsWith("Log.")) {
-      context.report(ISSUE, node, context.getLocation(node),
-          "Using 'Log' instead of 'Timber'", null);
+      context.report(ISSUE, node, context.getLocation(node), "Using 'Log' instead of 'Timber'",
+          null);
     }
   }
 
-  public static final Issue ISSUE = Issue.create(
-      "LogNotTimber",
-      "Logging call to Log instead of Timber",
-      "This check looks through all the logging calls for instances where the Android Log " +
-          "class was used instead of Timber.",
-      "Since Timber is included in the project, it is likely that calls to Log should " +
-          "instead be going to Timber.",
-      Category.CORRECTNESS, 5, Severity.WARNING,
-      new Implementation(CallToLogNotTimberDetector.class, Scope.JAVA_FILE_SCOPE));
+  public static final Issue ISSUE =
+      Issue.create("LogNotTimber", "Logging call to Log instead of Timber",
+          "This check looks through all the logging calls for instances where the Android Log "
+              + "class was used instead of Timber.",
+          "Since Timber is included in the project, it is likely that calls to Log should "
+              + "instead be going to Timber.", Category.CORRECTNESS, 5, Severity.WARNING,
+          new Implementation(CallToLogNotTimberDetector.class, Scope.JAVA_FILE_SCOPE));
 }
--- timber-lint/src/main/java/timber/lint/IssueRegistry.java
+++ timber-lint/src/main/java/timber/lint/IssueRegistry.java
@@ -5,10 +5,8 @@ import com.android.tools.lint.detector.api.Issue;
 import java.util.Arrays;
 import java.util.List;
 
-public class IssueRegistry extends com.android.tools.lint.client.api.IssueRegistry {
-
-  @Override
-  public List<Issue> getIssues() {
+public final class IssueRegistry extends com.android.tools.lint.client.api.IssueRegistry {
+  @Override public List<Issue> getIssues() {
     return Arrays.asList(CallToLogNotTimberDetector.ISSUE);
   }
 }
