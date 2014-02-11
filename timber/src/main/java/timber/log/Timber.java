d8daf68887de4b66d148bbd6e237560e9189aeeb
Peter Attardo <Peter@Scopely.com>
2014-02-11 15:37:39 -0800

    Comment formatting

--- timber/src/main/java/timber/log/Timber.java
+++ timber/src/main/java/timber/log/Timber.java
@@ -203,8 +203,8 @@ public final class Timber {
       return tag.substring(tag.lastIndexOf('.') + 1);
     }
 
-    /** If no varargs are supplied, treat it as a request to log the string without formatting */
     private static String formatString(String message, Object... args) {
+      // If no varargs are supplied, treat it as a request to log the string without formatting
       return args.length == 0 ? message : String.format(message, args);
     }
 
