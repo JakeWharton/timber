0ae216f7ee83da1808e2252169d7e8423de9406e
Himanshu Virmani <himanshuvirmani@gmail.com>
2014-04-10 14:36:51 +0530

    Added ErrorWarningTree for production builds.

--- timber/src/main/java/timber/log/Timber.java
+++ timber/src/main/java/timber/log/Timber.java
@@ -252,6 +252,76 @@ public final class Timber {
       NEXT_TAG.set(tag);
     }
   }
+  
+  /** A {@link Tree} for debug builds. Automatically infers the tag from the calling class. */
+  public static class ErrorWarningTree implements TaggedTree {
+    private static final Pattern ANONYMOUS_CLASS = Pattern.compile("\\$\\d+$");
+    private static final ThreadLocal<String> NEXT_TAG = new ThreadLocal<String>();
+
+    private static String createTag() {
+      String tag = NEXT_TAG.get();
+      if (tag != null) {
+        NEXT_TAG.remove();
+        return tag;
+      }
+
+      tag = new Throwable().getStackTrace()[4].getClassName();
+      Matcher m = ANONYMOUS_CLASS.matcher(tag);
+      if (m.find()) {
+        tag = m.replaceAll("");
+      }
+      return tag.substring(tag.lastIndexOf('.') + 1);
+    }
+
+    static String formatString(String message, Object... args) {
+      // If no varargs are supplied, treat it as a request to log the string without formatting.
+      return args.length == 0 ? message : String.format(message, args);
+    }
+
+    @Override public void v(String message, Object... args) {
+      
+    }
+
+    @Override public void v(Throwable t, String message, Object... args) {
+      
+    }
+
+    @Override public void d(String message, Object... args) {
+      
+    }
+
+    @Override public void d(Throwable t, String message, Object... args) {
+      
+    }
+
+    @Override public void i(String message, Object... args) {
+      Log.i(createTag(), formatString(message, args));
+    }
+
+    @Override public void i(Throwable t, String message, Object... args) {
+      Log.i(createTag(), formatString(message, args), t);
+    }
+
+    @Override public void w(String message, Object... args) {
+      Log.w(createTag(), formatString(message, args));
+    }
+
+    @Override public void w(Throwable t, String message, Object... args) {
+      Log.w(createTag(), formatString(message, args), t);
+    }
+
+    @Override public void e(String message, Object... args) {
+      Log.e(createTag(), formatString(message, args));
+    }
+
+    @Override public void e(Throwable t, String message, Object... args) {
+      Log.e(createTag(), formatString(message, args), t);
+    }
+
+    @Override public void tag(String tag) {
+      NEXT_TAG.set(tag);
+    }
+  }
 
   /** A {@link Tree} which does nothing. Useful for extending. */
   public static class HollowTree implements Tree {
