9360d22452704c906128a86e93a42cf0228dda71
Jake Wharton <jw@squareup.com>
2015-02-17 17:49:45 -0800

    Switch back to Throwable to ensure consistent traces.

--- timber/src/main/java/timber/log/Timber.java
+++ timber/src/main/java/timber/log/Timber.java
@@ -274,12 +274,14 @@ public final class Timber {
         return tag;
       }
 
-      StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-      if (stackTrace.length < 7) {
+      // DO NOT switch this to Thread.getCurrentThread().getStackTrace(). The test will pass
+      // because Robolectric runs them on the JVM but on Android the elements are different.
+      StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+      if (stackTrace.length < 6) {
         throw new IllegalStateException(
             "Synthetic stacktrace didn't have enough elements: are you using proguard?");
       }
-      tag = stackTrace[6].getClassName();
+      tag = stackTrace[5].getClassName();
       Matcher m = ANONYMOUS_CLASS.matcher(tag);
       if (m.find()) {
         tag = m.replaceAll("");
