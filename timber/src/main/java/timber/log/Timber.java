b55a5763734e64cc58f7ada7f9d4a3c4a6919dc4
Hani <jhanihashemi@gmail.com>
2015-04-05 23:08:16 +0430

    Add wtf method

--- timber/src/main/java/timber/log/Timber.java
+++ timber/src/main/java/timber/log/Timber.java
@@ -59,6 +59,16 @@ public final class Timber {
     TREE_OF_SOULS.e(t, message, args);
   }
 
+    /** Log an assert message with optional format args. */
+    public static void wtf(String message, Object... args) {
+        TREE_OF_SOULS.wtf(message, args);
+    }
+
+    /** Log an assert exception and a message with optional format args. */
+    public static void wtf(Throwable t, String message, Object... args) {
+        TREE_OF_SOULS.wtf(t, message, args);
+    }
+
   /**
    * A view into Timber's planted trees as a tree itself. This can be used for injecting a logger
    * instance rather than using static methods or to facilitate testing.
@@ -191,6 +201,22 @@ public final class Timber {
         forest.get(i).e(t, message, args);
       }
     }
+
+      @Override public void wtf(String message, Object... args) {
+          List<Tree> forest = FOREST;
+          //noinspection ForLoopReplaceableByForEach
+          for (int i = 0, count = forest.size(); i < count; i++) {
+              forest.get(i).wtf(message, args);
+          }
+      }
+
+      @Override public void wtf(Throwable t, String message, Object... args) {
+          List<Tree> forest = FOREST;
+          //noinspection ForLoopReplaceableByForEach
+          for (int i = 0, count = forest.size(); i < count; i++) {
+              forest.get(i).wtf(t, message, args);
+          }
+      }
   };
 
   private Timber() {
@@ -228,6 +254,12 @@ public final class Timber {
 
     /** Log an error exception and a message with optional format args. */
     void e(Throwable t, String message, Object... args);
+
+      /** Log an assert message with optional format args. */
+      void wtf(String message, Object... args);
+
+      /** Log an assert exception and a message with optional format args. */
+      void wtf(Throwable t, String message, Object... args);
   }
 
   /** A facade for attaching tags to logging calls. Install instances via {@link #plant} */
@@ -334,6 +366,16 @@ public final class Timber {
       throwShade(Log.ERROR, maybeFormat(message, args), t);
     }
 
+      @Override
+      public final void wtf(String message, Object... args) {
+          throwShade(Log.ASSERT, maybeFormat(message, args), null);
+      }
+
+      @Override
+      public final void wtf(Throwable t, String message, Object... args) {
+          throwShade(Log.ASSERT, maybeFormat(message, args), t);
+      }
+
     private void throwShade(int priority, String message, Throwable t) {
       if (message == null || message.length() == 0) {
         if (t == null) {
@@ -399,5 +441,11 @@ public final class Timber {
 
     @Override public void e(Throwable t, String message, Object... args) {
     }
+
+      @Override public void wtf(String message, Object... args) {
+      }
+
+      @Override public void wtf(Throwable t, String message, Object... args) {
+      }
   }
 }
