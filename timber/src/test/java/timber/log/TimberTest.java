b116d02f50ac02a2e59e4797f1100c18184cec50
Prateek Srivastava <f2prateek@gmail.com>
2016-07-05 16:41:01 -0600

    Add Timber.plant(Tree... trees) method to plant multiple trees

--- timber/src/test/java/timber/log/TimberTest.java
+++ timber/src/test/java/timber/log/TimberTest.java
@@ -49,6 +49,12 @@ public class TimberTest {
     } catch (IllegalArgumentException e) {
       assertThat(e).hasMessage("Cannot plant Timber into itself.");
     }
+    try {
+      Timber.plant(new Timber.Tree[] { timber });
+      fail();
+    } catch (IllegalArgumentException e) {
+      assertThat(e).hasMessage("Cannot plant Timber into itself.");
+    }
   }
 
   @Test public void treeCount() {
@@ -62,12 +68,32 @@ public class TimberTest {
     assertThat(Timber.treeCount()).isEqualTo(0);
   }
 
+  @SuppressWarnings("ConstantConditions")
   @Test public void nullTree() {
+    Timber.Tree nullTree = null;
     try {
-      Timber.plant(null);
+      Timber.plant(nullTree);
       fail();
     } catch (NullPointerException e) {
       assertThat(e).hasMessage("tree == null");
+    }
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test public void nullTreeArray() {
+    Timber.Tree[] nullTrees = null;
+    try {
+      Timber.plant(nullTrees);
+      fail();
+    } catch (NullPointerException e) {
+      assertThat(e).hasMessage("trees == null");
+    }
+    nullTrees = new Timber.Tree[] { null };
+    try {
+      Timber.plant(nullTrees);
+      fail();
+    } catch (NullPointerException e) {
+      assertThat(e).hasMessage("trees contains null");
     }
   }
 
@@ -76,6 +102,14 @@ public class TimberTest {
     Timber.DebugTree tree2 = new Timber.DebugTree();
     Timber.plant(tree1);
     Timber.plant(tree2);
+
+    assertThat(Timber.forest()).containsExactly(tree1, tree2);
+  }
+
+  @Test public void forestReturnsAllTreesPlanted() {
+    Timber.DebugTree tree1 = new Timber.DebugTree();
+    Timber.DebugTree tree2 = new Timber.DebugTree();
+    Timber.plant(tree1, tree2);
 
     assertThat(Timber.forest()).containsExactly(tree1, tree2);
   }
