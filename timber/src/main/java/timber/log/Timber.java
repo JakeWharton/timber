28489973f73374080d8ebf4a7c4414d221b6f337
dtunctuncer <dtunctuncer@gmail.com>
2017-10-26 09:26:29 +0300

    #260, Fixes kotlin non-null bug

--- timber/src/main/java/timber/log/Timber.java
+++ timber/src/main/java/timber/log/Timber.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.util.Log;
 
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -17,198 +18,10 @@ import static java.util.Collections.unmodifiableList;
 
 /** Logging for lazy people. */
 public final class Timber {
-  /** Log a verbose message with optional format args. */
-  public static void v(@NonNls String message, Object... args) {
-    TREE_OF_SOULS.v(message, args);
-  }
-
-  /** Log a verbose exception and a message with optional format args. */
-  public static void v(Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.v(t, message, args);
-  }
-
-  /** Log a verbose exception. */
-  public static void v(Throwable t) {
-    TREE_OF_SOULS.v(t);
-  }
-
-  /** Log a debug message with optional format args. */
-  public static void d(@NonNls String message, Object... args) {
-    TREE_OF_SOULS.d(message, args);
-  }
-
-  /** Log a debug exception and a message with optional format args. */
-  public static void d(Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.d(t, message, args);
-  }
-
-  /** Log a debug exception. */
-  public static void d(Throwable t) {
-    TREE_OF_SOULS.d(t);
-  }
-
-  /** Log an info message with optional format args. */
-  public static void i(@NonNls String message, Object... args) {
-    TREE_OF_SOULS.i(message, args);
-  }
-
-  /** Log an info exception and a message with optional format args. */
-  public static void i(Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.i(t, message, args);
-  }
-
-  /** Log an info exception. */
-  public static void i(Throwable t) {
-    TREE_OF_SOULS.i(t);
-  }
-
-  /** Log a warning message with optional format args. */
-  public static void w(@NonNls String message, Object... args) {
-    TREE_OF_SOULS.w(message, args);
-  }
-
-  /** Log a warning exception and a message with optional format args. */
-  public static void w(Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.w(t, message, args);
-  }
-
-  /** Log a warning exception. */
-  public static void w(Throwable t) {
-    TREE_OF_SOULS.w(t);
-  }
-
-  /** Log an error message with optional format args. */
-  public static void e(@NonNls String message, Object... args) {
-    TREE_OF_SOULS.e(message, args);
-  }
-
-  /** Log an error exception and a message with optional format args. */
-  public static void e(Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.e(t, message, args);
-  }
-
-  /** Log an error exception. */
-  public static void e(Throwable t) {
-    TREE_OF_SOULS.e(t);
-  }
-
-  /** Log an assert message with optional format args. */
-  public static void wtf(@NonNls String message, Object... args) {
-    TREE_OF_SOULS.wtf(message, args);
-  }
-
-  /** Log an assert exception and a message with optional format args. */
-  public static void wtf(Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.wtf(t, message, args);
-  }
-
-  /** Log an assert exception. */
-  public static void wtf(Throwable t) {
-    TREE_OF_SOULS.wtf(t);
-  }
-
-  /** Log at {@code priority} a message with optional format args. */
-  public static void log(int priority, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.log(priority, message, args);
-  }
-
-  /** Log at {@code priority} an exception and a message with optional format args. */
-  public static void log(int priority, Throwable t, @NonNls String message, Object... args) {
-    TREE_OF_SOULS.log(priority, t, message, args);
-  }
-
-  /** Log at {@code priority} an exception. */
-  public static void log(int priority, Throwable t) {
-    TREE_OF_SOULS.log(priority, t);
-  }
-
-  /**
-   * A view into Timber's planted trees as a tree itself. This can be used for injecting a logger
-   * instance rather than using static methods or to facilitate testing.
-   */
-  public static Tree asTree() {
-    return TREE_OF_SOULS;
-  }
-
-  /** Set a one-time tag for use on the next logging call. */
-  public static Tree tag(String tag) {
-    Tree[] forest = forestAsArray;
-    //noinspection ForLoopReplaceableByForEach
-    for (int i = 0, count = forest.length; i < count; i++) {
-      forest[i].explicitTag.set(tag);
-    }
-    return TREE_OF_SOULS;
-  }
-
-  /** Add a new logging tree. */
-  public static void plant(Tree tree) {
-    if (tree == null) {
-      throw new NullPointerException("tree == null");
-    }
-    if (tree == TREE_OF_SOULS) {
-      throw new IllegalArgumentException("Cannot plant Timber into itself.");
-    }
-    synchronized (FOREST) {
-      FOREST.add(tree);
-      forestAsArray = FOREST.toArray(new Tree[FOREST.size()]);
-    }
-  }
-
-  /** Adds new logging trees. */
-  public static void plant(Tree... trees) {
-    if (trees == null) {
-      throw new NullPointerException("trees == null");
-    }
-    for (Tree tree : trees) {
-      if (tree == null) {
-        throw new NullPointerException("trees contains null");
-      }
-      if (tree == TREE_OF_SOULS) {
-        throw new IllegalArgumentException("Cannot plant Timber into itself.");
-      }
-    }
-    synchronized (FOREST) {
-      Collections.addAll(FOREST, trees);
-      forestAsArray = FOREST.toArray(new Tree[FOREST.size()]);
-    }
-  }
-
-  /** Remove a planted tree. */
-  public static void uproot(Tree tree) {
-    synchronized (FOREST) {
-      if (!FOREST.remove(tree)) {
-        throw new IllegalArgumentException("Cannot uproot tree which is not planted: " + tree);
-      }
-      forestAsArray = FOREST.toArray(new Tree[FOREST.size()]);
-    }
-  }
-
-  /** Remove all planted trees. */
-  public static void uprootAll() {
-    synchronized (FOREST) {
-      FOREST.clear();
-      forestAsArray = TREE_ARRAY_EMPTY;
-    }
-  }
-
-  /** Return a copy of all planted {@linkplain Tree trees}. */
-  public static List<Tree> forest() {
-    synchronized (FOREST) {
-      return unmodifiableList(new ArrayList<>(FOREST));
-    }
-  }
-
-  public static int treeCount() {
-    synchronized (FOREST) {
-      return FOREST.size();
-    }
-  }
-
   private static final Tree[] TREE_ARRAY_EMPTY = new Tree[0];
   // Both fields guarded by 'FOREST'.
   private static final List<Tree> FOREST = new ArrayList<>();
   static volatile Tree[] forestAsArray = TREE_ARRAY_EMPTY;
-
   /** A {@link Tree} that delegates to all planted trees in the {@linkplain #FOREST forest}. */
   private static final Tree TREE_OF_SOULS = new Tree() {
     @Override public void v(String message, Object... args) {
@@ -387,6 +200,247 @@ public final class Timber {
   private Timber() {
     throw new AssertionError("No instances.");
   }
+
+    /**
+     * Log a verbose message with optional format args.
+     */
+    public static void v(@NonNls String message, Object... args) {
+        TREE_OF_SOULS.v(message, args);
+    }
+
+    /**
+     * Log a verbose exception and a message with optional format args.
+     */
+    public static void v(Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.v(t, message, args);
+    }
+
+    /**
+     * Log a verbose exception.
+     */
+    public static void v(Throwable t) {
+        TREE_OF_SOULS.v(t);
+    }
+
+    /**
+     * Log a debug message with optional format args.
+     */
+    public static void d(@NonNls String message, Object... args) {
+        TREE_OF_SOULS.d(message, args);
+    }
+
+    /**
+     * Log a debug exception and a message with optional format args.
+     */
+    public static void d(Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.d(t, message, args);
+    }
+
+    /**
+     * Log a debug exception.
+     */
+    public static void d(Throwable t) {
+        TREE_OF_SOULS.d(t);
+    }
+
+    /**
+     * Log an info message with optional format args.
+     */
+    public static void i(@NonNls String message, Object... args) {
+        TREE_OF_SOULS.i(message, args);
+    }
+
+    /**
+     * Log an info exception and a message with optional format args.
+     */
+    public static void i(Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.i(t, message, args);
+    }
+
+    /**
+     * Log an info exception.
+     */
+    public static void i(Throwable t) {
+        TREE_OF_SOULS.i(t);
+    }
+
+    /**
+     * Log a warning message with optional format args.
+     */
+    public static void w(@NonNls String message, Object... args) {
+        TREE_OF_SOULS.w(message, args);
+    }
+
+    /**
+     * Log a warning exception and a message with optional format args.
+     */
+    public static void w(Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.w(t, message, args);
+    }
+
+    /**
+     * Log a warning exception.
+     */
+    public static void w(Throwable t) {
+        TREE_OF_SOULS.w(t);
+    }
+
+    /**
+     * Log an error message with optional format args.
+     */
+    public static void e(@NonNls String message, Object... args) {
+        TREE_OF_SOULS.e(message, args);
+    }
+
+    /**
+     * Log an error exception and a message with optional format args.
+     */
+    public static void e(Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.e(t, message, args);
+    }
+
+    /**
+     * Log an error exception.
+     */
+    public static void e(Throwable t) {
+        TREE_OF_SOULS.e(t);
+    }
+
+    /**
+     * Log an assert message with optional format args.
+     */
+    public static void wtf(@NonNls String message, Object... args) {
+        TREE_OF_SOULS.wtf(message, args);
+    }
+
+    /**
+     * Log an assert exception and a message with optional format args.
+     */
+    public static void wtf(Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.wtf(t, message, args);
+    }
+
+    /**
+     * Log an assert exception.
+     */
+    public static void wtf(Throwable t) {
+        TREE_OF_SOULS.wtf(t);
+    }
+
+    /**
+     * Log at {@code priority} a message with optional format args.
+     */
+    public static void log(int priority, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.log(priority, message, args);
+    }
+
+    /**
+     * Log at {@code priority} an exception and a message with optional format args.
+     */
+    public static void log(int priority, Throwable t, @NonNls String message, Object... args) {
+        TREE_OF_SOULS.log(priority, t, message, args);
+    }
+
+    /**
+     * Log at {@code priority} an exception.
+     */
+    public static void log(int priority, Throwable t) {
+        TREE_OF_SOULS.log(priority, t);
+    }
+
+    /**
+     * A view into Timber's planted trees as a tree itself. This can be used for injecting a logger
+     * instance rather than using static methods or to facilitate testing.
+     */
+    public static Tree asTree() {
+        return TREE_OF_SOULS;
+    }
+
+    /**
+     * Set a one-time tag for use on the next logging call.
+     */
+    public static Tree tag(String tag) {
+        Tree[] forest = forestAsArray;
+        //noinspection ForLoopReplaceableByForEach
+        for (int i = 0, count = forest.length; i < count; i++) {
+            forest[i].explicitTag.set(tag);
+        }
+        return TREE_OF_SOULS;
+    }
+
+    /**
+     * Add a new logging tree.
+     */
+    public static void plant(Tree tree) {
+        if (tree == null) {
+            throw new NullPointerException("tree == null");
+        }
+        if (tree == TREE_OF_SOULS) {
+            throw new IllegalArgumentException("Cannot plant Timber into itself.");
+        }
+        synchronized (FOREST) {
+            FOREST.add(tree);
+            forestAsArray = FOREST.toArray(new Tree[FOREST.size()]);
+        }
+    }
+
+    /**
+     * Adds new logging trees.
+     */
+    public static void plant(Tree... trees) {
+        if (trees == null) {
+            throw new NullPointerException("trees == null");
+        }
+        for (Tree tree : trees) {
+            if (tree == null) {
+                throw new NullPointerException("trees contains null");
+            }
+            if (tree == TREE_OF_SOULS) {
+                throw new IllegalArgumentException("Cannot plant Timber into itself.");
+            }
+        }
+        synchronized (FOREST) {
+            Collections.addAll(FOREST, trees);
+            forestAsArray = FOREST.toArray(new Tree[FOREST.size()]);
+        }
+    }
+
+    /**
+     * Remove a planted tree.
+     */
+    public static void uproot(Tree tree) {
+        synchronized (FOREST) {
+            if (!FOREST.remove(tree)) {
+                throw new IllegalArgumentException("Cannot uproot tree which is not planted: " + tree);
+            }
+            forestAsArray = FOREST.toArray(new Tree[FOREST.size()]);
+        }
+    }
+
+    /**
+     * Remove all planted trees.
+     */
+    public static void uprootAll() {
+        synchronized (FOREST) {
+            FOREST.clear();
+            forestAsArray = TREE_ARRAY_EMPTY;
+        }
+    }
+
+    /**
+     * Return a copy of all planted {@linkplain Tree trees}.
+     */
+    public static List<Tree> forest() {
+        synchronized (FOREST) {
+            return unmodifiableList(new ArrayList<>(FOREST));
+        }
+    }
+
+    public static int treeCount() {
+        synchronized (FOREST) {
+            return FOREST.size();
+        }
+    }
 
   /** A facade for handling logging calls. Install instances via {@link #plant Timber.plant()}. */
   public static abstract class Tree {
@@ -615,17 +669,17 @@ public final class Timber {
         throw new IllegalStateException(
             "Synthetic stacktrace didn't have enough elements: are you using proguard?");
       }
-      return createStackElementTag(stackTrace[CALL_STACK_INDEX]);
+        return createStackElementTag(stackTrace[CALL_STACK_INDEX]);
     }
 
-    /**
-     * Break up {@code message} into maximum-length chunks (if needed) and send to either
+      /**
+       * Break up {@code message} into maximum-length chunks (if needed) and send to either
      * {@link Log#println(int, String, String) Log.println()} or
      * {@link Log#wtf(String, String) Log.wtf()} for logging.
      *
      * {@inheritDoc}
      */
-    @Override protected void log(int priority, String tag, String message, Throwable t) {
+    @Override protected void log(int priority, @Nullable String tag, String message, Throwable t) {
       if (message.length() < MAX_LOG_LENGTH) {
         if (priority == Log.ASSERT) {
           Log.wtf(tag, message);
