06b0ab1342cff6a5f36923eb8b479a73ae005c77
HughIngram <HughIngram@users.noreply.github.com>
2018-03-04 20:01:06 +0000

    initial implementation of logging with metadata

--- timber-sample/src/main/java/com/example/timber/FakeCrashLibrary.java
+++ timber-sample/src/main/java/com/example/timber/FakeCrashLibrary.java
@@ -1,9 +1,15 @@
 package com.example.timber;
 
+import java.util.Map;
+
 /** Not a real crash reporting library! */
 public final class FakeCrashLibrary {
   public static void log(int priority, String tag, String message) {
     // TODO add log entry to circular buffer.
+  }
+
+  public static void log(int priority, String message, Map<String, Object> metadata) {
+    // TODO add log entry with metadata
   }
 
   public static void logWarning(Throwable t) {
--- timber-sample/src/main/java/com/example/timber/ui/DemoActivity.java
+++ timber-sample/src/main/java/com/example/timber/ui/DemoActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.example.timber.R;
@@ -24,6 +27,10 @@ public class DemoActivity extends Activity {
   @OnClick({ R.id.hello, R.id.hey, R.id.hi })
   public void greetingClicked(Button button) {
     Timber.i("A button with ID %s was clicked to say '%s'.", button.getId(), button.getText());
+    Map<String, Object> eventMetadata = new HashMap<>();
+    eventMetadata.put("event-type", "button-pressed");
+    eventMetadata.put("button-id", String.valueOf(button.getId()));
+    Timber.v(eventMetadata, "This is an example of a structured log");
     Toast.makeText(this, "Check logcat for a greeting!", LENGTH_SHORT).show();
   }
 }
