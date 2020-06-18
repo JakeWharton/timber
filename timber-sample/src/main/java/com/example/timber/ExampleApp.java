1b532975773d60c9e623ff9e3f216168f0fbc643
Manoel Aranda Neto <marandaneto@gmail.com>
2020-06-18 15:24:05 +0200

    remove comments

--- timber-sample/src/main/java/com/example/timber/ExampleApp.java
+++ timber-sample/src/main/java/com/example/timber/ExampleApp.java
@@ -21,9 +21,6 @@ public class ExampleApp extends Application {
   /** A tree which logs important information for crash reporting. */
   private static class CrashReportingTree extends Timber.Tree {
     CrashReportingTree() {
-      // attachStackTraceString=false
-      // usually crash reporting tools already gets stack traces in a structured way,
-      // because they might need to be demangled.
       super(false);
     }
 
