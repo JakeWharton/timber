9b92fc004c85032fc6eafa83455b86a41d928627
Manoel Aranda Neto <marandaneto@gmail.com>
2020-06-18 15:18:23 +0200

    feat: adding an attach stacktrace option

--- timber-sample/src/main/java/com/example/timber/ExampleApp.java
+++ timber-sample/src/main/java/com/example/timber/ExampleApp.java
@@ -20,6 +20,13 @@ public class ExampleApp extends Application {
 
   /** A tree which logs important information for crash reporting. */
   private static class CrashReportingTree extends Timber.Tree {
+    CrashReportingTree() {
+      // attachStackTraceString=false
+      // usually crash reporting tools already gets stack traces in a structured way,
+      // because they might need to be demangled.
+      super(false);
+    }
+
     @Override protected void log(int priority, String tag, @NonNull String message, Throwable t) {
       if (priority == Log.VERBOSE || priority == Log.DEBUG) {
         return;
