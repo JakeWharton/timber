b22f02fceb87ce3812826edc2645f5fe486e92e6
sinan.kozak <sinan.kozak@deliveryhero.com>
2025-02-17 14:32:48 +0100

    Check timber.log.Timber.Forest and upgrade new functions

--- timber-lint/src/main/java/timber/lint/WrongTimberUsageDetector.kt
+++ timber-lint/src/main/java/timber/lint/WrongTimberUsageDetector.kt
@@ -1,31 +1,7 @@
 package timber.lint
 
-import com.android.tools.lint.detector.api.skipParentheses
-import org.jetbrains.uast.util.isMethodCall
-import com.android.tools.lint.detector.api.minSdkLessThan
-import com.android.tools.lint.detector.api.isString
-import com.android.tools.lint.detector.api.isKotlin
-import org.jetbrains.uast.isInjectionHost
-import org.jetbrains.uast.evaluateString
-import com.android.tools.lint.detector.api.Detector
-import com.android.tools.lint.detector.api.JavaContext
-import org.jetbrains.uast.UCallExpression
-import com.intellij.psi.PsiMethod
-import com.android.tools.lint.client.api.JavaEvaluator
-import com.android.tools.lint.detector.api.LintFix
-import org.jetbrains.uast.UElement
-import org.jetbrains.uast.UMethod
-import org.jetbrains.uast.UExpression
-import com.android.tools.lint.detector.api.Incident
-import org.jetbrains.uast.UQualifiedReferenceExpression
-import org.jetbrains.uast.UBinaryExpression
-import org.jetbrains.uast.UastBinaryOperator
-import org.jetbrains.uast.UIfExpression
-import com.intellij.psi.PsiMethodCallExpression
-import com.intellij.psi.PsiLiteralExpression
-import com.intellij.psi.PsiType
-import com.intellij.psi.PsiClassType
 import com.android.tools.lint.checks.StringFormatDetector
+import com.android.tools.lint.client.api.JavaEvaluator
 import com.android.tools.lint.client.api.TYPE_BOOLEAN
 import com.android.tools.lint.client.api.TYPE_BOOLEAN_WRAPPER
 import com.android.tools.lint.client.api.TYPE_BYTE
@@ -47,25 +23,57 @@ import com.android.tools.lint.client.api.TYPE_STRING
 import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
 import com.android.tools.lint.detector.api.Category.Companion.MESSAGES
 import com.android.tools.lint.detector.api.ConstantEvaluator.evaluateString
+import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.Detector.UastScanner
 import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Incident
 import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.LintFix
 import com.android.tools.lint.detector.api.Scope.Companion.JAVA_FILE_SCOPE
 import com.android.tools.lint.detector.api.Severity.ERROR
 import com.android.tools.lint.detector.api.Severity.WARNING
-import org.jetbrains.uast.ULiteralExpression
-import org.jetbrains.uast.USimpleNameReferenceExpression
+import com.android.tools.lint.detector.api.isKotlin
+import com.android.tools.lint.detector.api.isString
+import com.android.tools.lint.detector.api.minSdkLessThan
+import com.intellij.psi.PsiClassType
 import com.intellij.psi.PsiField
+import com.intellij.psi.PsiLiteralExpression
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.PsiParameter
+import com.intellij.psi.PsiType
+import com.intellij.psi.PsiTypes
+import org.jetbrains.uast.UBinaryExpression
+import org.jetbrains.uast.UCallExpression
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.UIfExpression
+import org.jetbrains.uast.ULiteralExpression
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.UQualifiedReferenceExpression
+import org.jetbrains.uast.USimpleNameReferenceExpression
+import org.jetbrains.uast.UastBinaryOperator
+import org.jetbrains.uast.evaluateString
+import org.jetbrains.uast.isInjectionHost
+import org.jetbrains.uast.skipParenthesizedExprUp
+import org.jetbrains.uast.util.isMethodCall
 import java.lang.Byte
 import java.lang.Double
 import java.lang.Float
-import java.lang.IllegalStateException
 import java.lang.Long
 import java.lang.Short
 import java.util.Calendar
 import java.util.Date
 import java.util.regex.Pattern
+import kotlin.Any
+import kotlin.Boolean
+import kotlin.IllegalStateException
+import kotlin.Int
+import kotlin.Number
+import kotlin.String
+import kotlin.Throwable
+import kotlin.arrayOf
 
 class WrongTimberUsageDetector : Detector(), UastScanner {
   override fun getApplicableMethodNames() = listOf("tag", "format", "v", "d", "i", "w", "e", "wtf")
@@ -107,13 +115,14 @@ class WrongTimberUsageDetector : Detector(), UastScanner {
   private fun isTimberLogMethod(method: PsiMethod, evaluator: JavaEvaluator): Boolean {
     return evaluator.isMemberInClass(method, "timber.log.Timber")
         || evaluator.isMemberInClass(method, "timber.log.Timber.Companion")
+        || evaluator.isMemberInClass(method, "timber.log.Timber.Forest") // This is the real fix
         || evaluator.isMemberInClass(method, "timber.log.Timber.Tree")
   }
 
   private fun checkNestedStringFormat(context: JavaContext, call: UCallExpression) {
     var current: UElement? = call
     while (true) {
-      current = skipParentheses(current!!.uastParent)
+      current = skipParenthesizedExprUp(current!!.uastParent)
       if (current == null || current is UMethod) {
         // Reached AST root or code block node; String.format not inside Timber.X(..).
         return
@@ -216,7 +225,7 @@ class WrongTimberUsageDetector : Detector(), UastScanner {
 
       val type = getType(argument) ?: continue
       val last = formatType.last()
-      if (formatType.length >= 2 && formatType[formatType.length - 2].toLowerCase() == 't') {
+      if (formatType.length >= 2 && formatType[formatType.length - 2].lowercaseChar() == 't') {
         // Date time conversion.
         when (last) {
           'H', 'I', 'k', 'l', 'M', 'S', 'L', 'N', 'p', 'z', 'Z', 's', 'Q', // time
@@ -288,11 +297,11 @@ class WrongTimberUsageDetector : Detector(), UastScanner {
       val expressionType = literalExpression.type
       when {
         isString(expressionType!!) -> return String::class.java
-        expressionType === PsiType.INT -> return Integer.TYPE
-        expressionType === PsiType.FLOAT -> return java.lang.Float.TYPE
-        expressionType === PsiType.CHAR -> return Character.TYPE
-        expressionType === PsiType.BOOLEAN -> return java.lang.Boolean.TYPE
-        expressionType === PsiType.NULL -> return Any::class.java
+        expressionType === PsiTypes.intType() -> return Integer.TYPE
+        expressionType === PsiTypes.floatType() -> return java.lang.Float.TYPE
+        expressionType === PsiTypes.charType() -> return Character.TYPE
+        expressionType === PsiTypes.booleanType() -> return java.lang.Boolean.TYPE
+        expressionType === PsiTypes.nullType() -> return Any::class.java
       }
     }
 
